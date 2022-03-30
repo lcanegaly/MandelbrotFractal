7f67acc9ff565a88d66df4dac64e14c35a4c02c3
lcanegaly <64861946+lcanegaly@users.noreply.github.com>
2022-03-29 17:48:21 -0700

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/lee/Dev/Projects/MandelbrotFractal
-# Build directory: /home/lee/Dev/Projects/MandelbrotFractal/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("MandelbrotFractal/Source")
-subdirs("Dependencies/glfw-3.3.6")
